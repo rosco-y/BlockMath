d1ae655c0f08e3798c0cb8ae50f38147f55c3596
Ross Ylitalo <ross.ylitalo@gmail.com>
2017-12-30 19:35:59 -0500

    Now have a 1x1x1 "Item" that crawls across the counter.

---
+++ Assets/ItemMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovement : MonoBehaviour {
+
+    public Transform _thisItem;
+    // Use this for initialization
+    public float _speed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = _thisItem.transform.position;
+        pos = new Vector3(pos.x, pos.y, pos.z + _speed);
+        _thisItem.transform.position = pos;
+	}
+}
